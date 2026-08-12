b0a4447e8babdf0706f223eab42cdcde5e9c4630
MuhammadSabir329 <subhannaveed.329@gmail.com>
2026-08-12 16:52:54 +0500

    Fix broken image paths for production build

--- src/components/AllListsHidden.jsx
+++ src/components/AllListsHidden.jsx
@@ -3,7 +3,7 @@ import noListImage from "../assets/nolist.png";
 export default function AllListsHidden() {
   return (
     <div className="flex flex-col items-center justify-center h-full w-full gap-2">
-      <img className={noListImage} alt="" />
+      <img className="h-32 w-40" src={noListImage} alt="" />
       <span className="text-[18px] font-normal mt-5">All lists are hidden</span>
       <span className="text-sm">Select any list to see your tasks</span>
     </div>
